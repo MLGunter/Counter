9df7a9889ad0498e37e68c88b9d8c04317b21403
MLGunter <urazalievass@gmail.com>
2023-02-20 21:01:22 +0400

    Первая версия приложения "Счётчик": выведен lablel, в котором видно количество нажатий на кнопку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var counterIncreaseButton: UIButton!
+    
+    var counter: Int = 0
+    
+    @IBAction func counterIncreaseTap(_ sender: Any) {
+        counter = counter + 1
+        counterLabel.text = ("Значение счетчика: \(counter)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
